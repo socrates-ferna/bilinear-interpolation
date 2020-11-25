cc17fa6d907c4e6ffa42253cd3607cfb9516312c
mikidefts <100345872@alumnos.uc3m.es>
2020-11-25 14:15:21 +0100

    funcion interpolacion bilineal solo la operacion

--- main.f90
+++ main.f90
@@ -7,6 +7,9 @@ use iso_Fortran_env, only: real64
 real(real64) :: pxyin(2,2) !la primera es elsubindice y la segunda si es x o y
 real(rela64) :: fin(2,2), pxyout(2), fout(1)
 
-real(real64) :: dist11, dist12, dist21, dist22
 
-fout(1)  = (fin(1)*(pxyin(2,1)-pxyout(1))*(pxyin(2,2)-pxyout(2))/((pxyin(2,1)-pxyin(1,1))*(pxyin(2,2)-pxyin(1,2)))
+fout(1) = 1/((pxyin(2,1)-pxyin(1,1))*(pxyin(2,2)-pxyin(1,2)))*( &
+(fin(1,1)* (pxyin(2,1)-pxyout(1))*(pxyin(2,2)-pxyout(2)) ) + &
+(fin(2,1)*(-pxyin(1,1)+pxyout(1))*(pxyin(2,2)-pxyout(2)) ) + &
+(fin(1,2)*(pxyin(2,1)-pxyout(1))*(-pxyin(1,2)+pxyout(2)) ) + &
+(fin(2,2)*(-pxyin(1,1)+pxyout(1))*(-pxyin(1,2)+pxyout(2)) ) )
